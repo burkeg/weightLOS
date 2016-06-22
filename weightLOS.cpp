f789ba75febf3a4b37dd4f4a12706911930a1011
burkeg <burkeg@purdue.edu>
2016-06-22 13:19:45 -0700

    First Commit
    features:
    read file data from .csv into data structure
    quicksort data based on columns

---
+++ weightLOS.cpp
@@ -0,0 +1,25 @@
+/* Filename: weightLOS.cpp
+ * Author: Gabe and Paula
+ * Date: 06/16/16
+ * Description: contains the function main() written in c/cpp
+ */
+
+#include "weightLOS.h"
+
+/* Function: main()
+ * Prototype: main( int argc, char * argv[] );
+ * Description: 
+ * Error Conditions:
+ * Return: EXIT_FAILURE on failure, EXIT_SUCCESS on successs
+ */
+int main(int argc, char * argv[] ) {
+	vector<food_item> list;// = new food_item[959];
+	readFile("food_call.csv", &list);
+	quick_sort(&list, compare_by_Weight, 0, list.size()-1);
+	quick_sort(&list, compare_by_Carbohydrates, 0, list.size() - 1);
+	quick_sort(&list, compare_by_Amount, 0, list.size() - 1);
+	quick_sort(&list, compare_by_Protein, 0, list.size() - 1);
+	quick_sort(&list, compare_by_Saturated_Fat, 0, list.size() - 1);
+	
+	return 0;
+}
---
+++ weightLOS.h
@@ -0,0 +1,58 @@
+#ifndef WEIGHTLOS_H_
+#define WEIGHTLOS_H_
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <string>
+#pragma warning( disable : 4996 )
+
+using namespace std;
+//structs
+
+struct health_reg
+{
+	double min_fat;
+	double max_fat;
+	double food_energy;
+	double carbs_min;
+	double carbs_perc_min;
+	double carbs_perc_max;
+};
+struct food_item
+{
+	char *name;
+	double amount;
+	char *units;
+	double fat;
+	double food_energy;
+	double carbohydrates;
+	double protein;
+	double cholesterol;
+	double weight;
+	double saturated_fat;
+
+};
+//global varaibles
+
+//function prototypes
+int readFile(const char * filename, vector<food_item> * file_contents);
+void quick_sort(vector<food_item> * data, double compare(vector<food_item> * data, const void *, const void *, bool getA), int left, int right);
+
+double compare_by_Name(vector<food_item> * data, const void * a, const void * b, bool getA);
+double compare_by_Amount(vector<food_item> * data, const void * a, const void * b, bool getA);
+double compare_by_Units(vector<food_item> * data, const void * a, const void * b, bool getA);
+double compare_by_Food_Energy(vector<food_item> * data, const void * a, const void * b, bool getA);
+double compare_by_Carbohydrates(vector<food_item> * data, const void * a, const void * b, bool getA);
+double compare_by_Protein(vector<food_item> * data, const void * a, const void * b, bool getA);
+double compare_by_Cholesterol(vector<food_item> * data, const void * a, const void * b, bool getA);
+double compare_by_Weight(vector<food_item> * data, const void * a, const void * b, bool getA);
+double compare_by_Saturated_Fat(vector<food_item> * data, const void * a, const void * b, bool getA);
+
+
+
+
+#endif
+
